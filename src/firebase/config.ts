baf3404a41a3ad220bdfffe4e0a9755d8a5b19e6
Gabriel Anibaldi <gabrielanibaldi@gmail.com>
2025-11-12 19:05:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9280951984-8da98",
+  "appId": "1:503073015037:web:0e8b61155b6bd5f6ffea94",
+  "apiKey": "AIzaSyCsK1LVzbUmusUITar5pwd9N7lE1jJcbms",
+  "authDomain": "studio-9280951984-8da98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "503073015037"
+};
